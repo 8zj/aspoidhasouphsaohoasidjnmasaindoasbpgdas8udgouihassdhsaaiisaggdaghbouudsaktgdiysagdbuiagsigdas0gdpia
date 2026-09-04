1e4ac732a19d569ba2ca9c2e07f3befaabc4c08a
Pick <90311538+8zj@users.noreply.github.com>
2026-09-04 17:01:07 -0400

    Update Intermediate.lua

--- Strats/Intermediate.lua
+++ Strats/Intermediate.lua
@@ -1,1 +1,73 @@
-
+getgenv().PickHubLOL = {
+    Main = {
+        AutoStrat       = true,
+        AutoRestart     = false,
+        AutoReturnLobby = true,
+        AutoSkip        = false,
+        SendWebhook     = false,
+        TimeScale       = 2,
+    },
+    Mis = {
+        AntiLag    = false,
+        AutoPickup = true,
+        ANTIAFK    = true,
+        FreeCam    = false,
+    },
+    Marco = {
+        AutoDJ          = false,
+        Autocommander   = false,
+        APCSpam         = false,
+        AutoMercenary   = false,
+        AutoMilitary    = false,
+        AutoNecromancer = false,
+    },
+    Urls = {
+        Webhook  = "",
+        macroURL = "",
+    },
+    GameInfo = {
+        Enabled   = true,
+        MapIndex  = 1,
+        Mode      = "Intermediate",
+        Maps      = {
+            ["Cataclysm"] = {
+                Loadout = {"Soldier","Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/Intermediate/Cataclysm.lua"
+            },
+            ["Retro Crossroads"] = {
+                Loadout = {"Soldier","Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/Intermediate/Crossroads.lua"
+            },
+            ["Farm Lands"] = {
+                Loadout = {"Soldier","Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/Intermediate/FarmLands.lua"
+            },
+            ["Simplicity"] = {
+                Loadout = {"Soldier","Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/Intermediate/Simplicity.lua"
+            },
+            ["U-Turn"] = {
+                Loadout = {"Soldier","Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/Intermediate/U-Turn.lua"
+            },
+            ["Night Station"] = {
+                Loadout = {"Soldier","Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/Intermediate/Night-Station.lua"
+            },
+            ["Summer Castle"] = {
+                Loadout = {"Soldier","Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/Intermediate/Summer%20Castle.lua"
+            },
+            ["Sugar Rush"] = {
+                Loadout = {"Soldier","Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/Intermediate/Sugar%20Rush.lua"
+            },
+            ["Wrecked Battlefield"] = {
+                Loadout = {"Soldier","Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/Intermediate/Wrecked%20Battlefield.lua"
+            },
+        },
+        Modifiers = {}
+    },
+}
+loadstring(game:HttpGet("http://pickscripthub.xyz/api/execute/tds-multi-map"))()
