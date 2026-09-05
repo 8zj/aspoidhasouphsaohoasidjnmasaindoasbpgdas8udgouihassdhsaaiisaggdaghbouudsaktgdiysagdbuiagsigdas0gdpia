3656a12ba9027da70e490c3460356b4efcb22e00
Pick <90311538+8zj@users.noreply.github.com>
2026-09-04 20:49:57 -0400

    Create PizzaParty.lua

---
+++ Events/PizzaParty.lua
@@ -0,0 +1,69 @@
+-- [[
+-- Towers: Trapper, Shotgunner, None, None, None
+-- Mode: PizzaParty
+-- GameInfo: Unknown
+-- Map: Pizza Party
+-- Modifiers: None
+-- ]]
+
+-- [[ Wave 1 ]] --
+PSH:Place("Trapper", 4.494, 1.037, -30.543)
+PSH:Place("Trapper", 7.538, 1.037, -24.721)
+
+-- [[ Wave 2 ]] --
+PSH:Place("Trapper", 12.956, 1.037, -24.286)
+
+-- [[ Wave 4 ]] --
+PSH:Upgrade(3)
+PSH:Upgrade(2)
+PSH:Upgrade(1)
+
+-- [[ Wave 6 ]] --
+PSH:Place("Trapper", 12.570, 1.037, -19.182)
+PSH:Upgrade(4)
+
+-- [[ Wave 7 ]] --
+PSH:Place("Shotgunner", -1.077, 1.000, -37.077)
+
+-- [[ Wave 8 ]] --
+PSH:Upgrade(5)
+
+-- [[ Wave 9 ]] --
+PSH:Place("Shotgunner", 4.192, 1.000, -36.983)
+PSH:Upgrade(6)
+
+-- [[ Wave 10 ]] --
+PSH:Place("Shotgunner", 4.172, 1.037, -34.865)
+PSH:Upgrade(7)
+
+-- [[ Wave 11 ]] --
+PSH:Place("Shotgunner", -1.294, 1.037, -34.948)
+PSH:Sell(8)
+
+-- [[ Wave 12 ]] --
+PSH:Place("Shotgunner", -1.500, 1.037, -35.073)
+PSH:Upgrade(9)
+
+-- [[ Wave 13 ]] --
+PSH:Upgrade(9)
+PSH:Upgrade(5)
+
+-- [[ Wave 14 ]] --
+PSH:Upgrade(6)
+PSH:Upgrade(1)
+PSH:Upgrade(2)
+
+-- [[ Wave 15 ]] --
+PSH:Place("Trapper", 17.734, 1.037, -21.357)
+PSH:Upgrade(10)
+PSH:Upgrade(10)
+PSH:Place("Shotgunner", 8.549, 1.037, -19.606)
+PSH:Upgrade(11)
+
+-- [[ Wave 16 ]] --
+PSH:Upgrade(11)
+
+-- [[ Wave 17 ]] --
+PSH:Place("Shotgunner", 3.641, 1.037, -25.649)
+PSH:Upgrade(12)
+PSH:Upgrade(12)
