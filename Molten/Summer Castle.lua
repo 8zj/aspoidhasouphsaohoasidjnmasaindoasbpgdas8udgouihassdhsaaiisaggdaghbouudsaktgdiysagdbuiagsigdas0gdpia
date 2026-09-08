601556ec155608d0dd03992a3546af2995bcbaf9
Pick <90311538+8zj@users.noreply.github.com>
2026-09-07 22:09:38 -0400

    Add Summer Castle.lua to Molten directory

---
+++ Molten/Summer Castle.lua
@@ -0,0 +1,285 @@
+-- [[
+-- Towers: Cowboy, Minigunner, DJ Booth, Commander, Ranger
+-- Mode: Molten
+-- GameInfo: Unknown
+-- Map: Summer Castle
+-- Modifiers: None
+-- ]]
+
+-- [[ Wave 1 ]] --
+PSH:Place("Cowboy", 17.296, 0.987, 33.692)
+PSH:Upgrade(1)
+
+-- [[ Wave 2 ]] --
+PSH:Place("Cowboy", 17.655, 0.986, 31.160)
+PSH:Upgrade(2)
+
+-- [[ Wave 3 ]] --
+PSH:Place("Cowboy", 14.711, 0.984, 33.879)
+PSH:Upgrade(3)
+
+-- [[ Wave 4 ]] --
+PSH:Place("Cowboy", 15.048, 0.983, 31.309)
+PSH:Upgrade(4)
+
+-- [[ Wave 6 ]] --
+PSH:Upgrade(1)
+PSH:Upgrade(2)
+PSH:Upgrade(3)
+PSH:Upgrade(4)
+PSH:Place("Cowboy", 12.160, 0.996, 33.872)
+
+-- [[ Wave 7 ]] --
+PSH:Upgrade(5)
+PSH:Place("Cowboy", 12.514, 0.979, 31.377)
+PSH:Upgrade(6)
+PSH:Upgrade(6)
+PSH:Upgrade(5)
+
+-- [[ Wave 8 ]] --
+PSH:Place("Cowboy", 10.834, 0.975, 29.018)
+PSH:Place("Cowboy", 9.520, 0.972, 26.885)
+PSH:Place("Cowboy", 8.168, 0.968, 24.489)
+PSH:Upgrade(7)
+PSH:Upgrade(9)
+
+-- [[ Wave 9 ]] --
+PSH:Upgrade(8)
+PSH:Upgrade(7)
+PSH:Upgrade(8)
+PSH:Upgrade(9)
+
+-- [[ Wave 10 ]] --
+PSH:Upgrade(9)
+
+-- [[ Wave 11 ]] --
+PSH:Place("Minigunner", 6.495, 0.919, 9.377)
+PSH:Place("Minigunner", 9.332, 0.928, 10.627)
+PSH:Upgrade(11)
+PSH:Upgrade(10)
+PSH:Upgrade(10)
+
+-- [[ Wave 12 ]] --
+PSH:Place("DJ Booth", 2.625, 0.913, 8.037)
+PSH:Upgrade(12)
+PSH:Upgrade(12)
+
+-- [[ Wave 13 ]] --
+PSH:Upgrade(12)
+PSH:Upgrade(11)
+PSH:Place("Minigunner", 6.692, 0.909, 12.397)
+PSH:Upgrade(13)
+
+-- [[ Wave 14 ]] --
+PSH:Upgrade(13)
+PSH:Upgrade(7)
+PSH:Upgrade(6)
+
+-- [[ Wave 15 ]] --
+PSH:Upgrade(4)
+PSH:Upgrade(2)
+
+-- [[ Wave 16 ]] --
+PSH:Upgrade(1)
+PSH:Upgrade(1)
+PSH:Upgrade(4)
+PSH:Place("Minigunner", 12.434, 0.942, 10.903)
+PSH:Upgrade(14)
+PSH:Upgrade(14)
+PSH:Place("Minigunner", 9.587, 0.919, 13.678)
+PSH:Upgrade(15)
+PSH:Upgrade(15)
+
+-- [[ Wave 17 ]] --
+PSH:Upgrade(15)
+
+-- [[ Wave 18 ]] --
+PSH:Place("Commander", 3.514, 0.920, 13.495)
+PSH:Place("Commander", 13.262, 0.936, 13.826)
+PSH:Place("Commander", 10.236, 0.967, 19.788)
+PSH:Upgrade(17)
+PSH:Upgrade(16)
+PSH:Upgrade(18)
+PSH:Upgrade(18)
+PSH:Upgrade(17)
+PSH:Upgrade(16)
+
+-- [[ Wave 19 ]] --
+PSH:Upgrade(12)
+
+-- [[ Wave 20 ]] --
+PSH:Place("Minigunner", 7.085, 0.963, 19.716)
+PSH:Upgrade(19)
+PSH:Upgrade(19)
+PSH:Upgrade(9)
+PSH:Upgrade(8)
+PSH:Upgrade(8)
+PSH:Upgrade(7)
+PSH:Upgrade(6)
+PSH:Upgrade(3)
+
+-- [[ Wave 21 ]] --
+PSH:Upgrade(3)
+PSH:Upgrade(2)
+PSH:Upgrade(5)
+PSH:Upgrade(5)
+PSH:Upgrade(16)
+PSH:Upgrade(18)
+
+-- [[ Wave 22 ]] --
+PSH:Upgrade(17)
+PSH:Upgrade(19)
+PSH:Upgrade(14)
+PSH:Upgrade(11)
+PSH:Upgrade(10)
+
+-- [[ Wave 23 ]] --
+PSH:Place("Minigunner", 0.986, 0.930, 11.713)
+PSH:Place("Minigunner", -2.047, 0.929, 13.509)
+PSH:Upgrade(21)
+PSH:Upgrade(21)
+PSH:Upgrade(21)
+PSH:Upgrade(20)
+PSH:Upgrade(20)
+PSH:Upgrade(20)
+
+-- [[ Wave 24 ]] --
+PSH:Upgrade(13)
+
+-- [[ Wave 25 ]] --
+PSH:Upgrade(14)
+PSH:Place("Ranger", 10.886, 4.195, 8.366)
+PSH:Place("Ranger", 7.782, 4.313, 7.527)
+PSH:Place("Ranger", 13.420, 4.382, 6.757)
+PSH:Place("Ranger", 10.483, 4.467, 5.237)
+PSH:Upgrade(25)
+PSH:Upgrade(25)
+PSH:Upgrade(24)
+PSH:Upgrade(22)
+PSH:Upgrade(23)
+
+-- [[ Wave 26 ]] --
+PSH:Upgrade(24)
+PSH:Upgrade(22)
+PSH:Upgrade(23)
+PSH:Upgrade(12)
+PSH:Upgrade(9)
+
+-- [[ Wave 27 ]] --
+PSH:Upgrade(8)
+PSH:Upgrade(7)
+PSH:Upgrade(5)
+PSH:Upgrade(6)
+
+-- [[ Wave 28 ]] --
+PSH:Upgrade(3)
+PSH:Upgrade(4)
+PSH:Upgrade(1)
+PSH:Upgrade(2)
+PSH:Place("Ranger", 4.707, 4.714, 6.091)
+PSH:Upgrade(26)
+PSH:Upgrade(26)
+PSH:Place("Ranger", 7.152, 4.513, 4.042)
+PSH:Upgrade(27)
+
+-- [[ Wave 29 ]] --
+PSH:Upgrade(27)
+PSH:Place("Minigunner", 15.809, 0.961, 9.815)
+PSH:Upgrade(28)
+PSH:Upgrade(28)
+PSH:Upgrade(28)
+PSH:Upgrade(28)
+PSH:Place("Minigunner", 17.381, 0.977, 6.973)
+PSH:Place("Minigunner", 16.406, 0.981, 4.043)
+PSH:Place("Minigunner", 18.896, 0.975, 9.568)
+PSH:Place("Minigunner", 20.472, 0.988, 6.809)
+PSH:Place("Minigunner", 19.431, 0.988, 3.583)
+PSH:Upgrade(33)
+PSH:Upgrade(33)
+PSH:Upgrade(33)
+PSH:Upgrade(32)
+PSH:Upgrade(32)
+PSH:Upgrade(32)
+PSH:Upgrade(31)
+PSH:Upgrade(31)
+PSH:Upgrade(31)
+
+-- [[ Wave 30 ]] --
+PSH:Upgrade(29)
+PSH:Upgrade(29)
+PSH:Upgrade(29)
+PSH:Upgrade(17)
+PSH:Upgrade(18)
+PSH:Upgrade(16)
+PSH:Upgrade(11)
+PSH:Upgrade(19)
+
+-- [[ Wave 31 ]] --
+PSH:Upgrade(13)
+PSH:Upgrade(15)
+PSH:Upgrade(10)
+PSH:Upgrade(20)
+PSH:Upgrade(21)
+
+-- [[ Wave 32 ]] --
+PSH:Upgrade(24)
+PSH:Upgrade(22)
+PSH:Upgrade(25)
+PSH:Upgrade(27)
+PSH:Upgrade(23)
+PSH:Upgrade(26)
+PSH:Place("Minigunner", 16.277, 0.953, 12.977)
+PSH:Place("Minigunner", 19.541, 0.969, 12.652)
+PSH:Place("Minigunner", 16.365, 0.942, 16.148)
+PSH:Place("Minigunner", 19.445, 0.957, 16.144)
+PSH:Place("Minigunner", 17.871, 0.977, 18.746)
+
+-- [[ Wave 33 ]] --
+PSH:Place("Minigunner", 20.976, 0.981, 18.827)
+PSH:Place("Minigunner", 22.454, 0.970, 16.052)
+PSH:Upgrade(40)
+PSH:Upgrade(40)
+PSH:Upgrade(40)
+PSH:Upgrade(37)
+PSH:Upgrade(37)
+PSH:Upgrade(39)
+PSH:Upgrade(39)
+PSH:Upgrade(38)
+PSH:Upgrade(38)
+PSH:Upgrade(36)
+PSH:Upgrade(36)
+PSH:Upgrade(36)
+PSH:Upgrade(35)
+PSH:Upgrade(35)
+PSH:Upgrade(35)
+PSH:Upgrade(34)
+PSH:Upgrade(34)
+PSH:Upgrade(34)
+
+-- [[ Wave 34 ]] --
+PSH:Upgrade(34)
+PSH:Upgrade(35)
+PSH:Upgrade(37)
+PSH:Upgrade(37)
+PSH:Upgrade(36)
+PSH:Upgrade(38)
+PSH:Upgrade(38)
+PSH:Upgrade(39)
+PSH:Upgrade(39)
+PSH:Upgrade(24)
+PSH:Upgrade(22)
+PSH:Upgrade(29)
+PSH:Upgrade(30)
+PSH:Upgrade(30)
+PSH:Upgrade(30)
+PSH:Upgrade(30)
+PSH:Upgrade(33)
+PSH:Upgrade(32)
+PSH:Upgrade(31)
+PSH:Upgrade(40)
+
+-- [[ Wave 35 ]] --
+PSH:Upgrade(23)
+PSH:Upgrade(25)
+PSH:Upgrade(27)
+PSH:Upgrade(26)
