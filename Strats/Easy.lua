245ecef10cb34f15ce9776be4136ec514cbbd40b
Pick <90311538+8zj@users.noreply.github.com>
2026-09-08 09:53:29 -0400

    Update Easy.lua

--- Strats/Easy.lua
+++ Strats/Easy.lua
@@ -1,1 +1,57 @@
-
+getgenv().PickHubLOL = {
+    Main = {
+        AutoStrat       = true,
+        AutoRestart     = false,
+        AutoReturnLobby = true,
+        AutoSkip        = false,
+        SendWebhook     = false,
+        TimeScale       = 2,
+    },
+    Mis = {
+        AntiLag    = false,
+        AutoPickup = true,
+        ANTIAFK    = true,
+        FreeCam    = false,
+    },
+    Marco = {
+        AutoDJ          = false,
+        Autocommander   = false,
+        APCSpam         = false,
+        AutoMercenary   = false,
+        AutoMilitary    = false,
+        AutoNecromancer = false,
+    },
+    Urls = {
+        Webhook  = "",
+        macroURL = "",
+    },
+    GameInfo = {
+        Enabled   = true,
+        MapIndex  = 1,
+        Mode      = "Easy",
+        Maps      = {
+            ["U-Turn"] = {
+                Loadout = {"Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/EasyMode/map1.lua"
+            },
+            ["Grass Isle"] = {
+                Loadout = {"Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/EasyMode/map2.lua"
+            },
+            ["Crossroads"] = {
+                Loadout = {"Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/EasyMode/map3.lua"
+            },
+            ["Simplicity"] = {
+                loadout = {"Scout"},
+                URL = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/EasyMode/map4.lua"
+            },
+            ["Forest Camp"] = {
+                loadout = {"Scout"},
+                URL = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/EasyMode/map5.lua"
+            },
+        },
+        Modifiers = {}
+    },
+}
+loadstring(game:HttpGet("http://pickscripthub.xyz/api/execute/tds-multi-map"))()
