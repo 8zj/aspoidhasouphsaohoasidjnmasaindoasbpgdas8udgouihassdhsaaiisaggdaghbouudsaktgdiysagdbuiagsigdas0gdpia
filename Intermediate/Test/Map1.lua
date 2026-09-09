48f175d03ec521e7c35f33e0897fbd26d2da9a00
Pick <90311538+8zj@users.noreply.github.com>
2026-09-08 21:00:54 -0400

    Create Map1.lua

---
+++ Intermediate/Test/Map1.lua
@@ -0,0 +1,318 @@
+-- [[
+-- Towers: Shotgunner, Soldier, None, None, None
+-- Mode: Intermediate
+-- GameInfo: Unknown
+-- Map: Summer Castle
+-- Modifiers: None
+-- ]]
+
+-- [[ Wave 1 ]] --
+PSH:Place("Soldier", 24.075, 0.987, 28.791)
+PSH:Place("Soldier", 27.257, 0.983, 28.914)
+PSH:Upgrade(2)
+PSH:Upgrade(1)
+
+-- [[ Wave 2 ]] --
+PSH:Place("Soldier", 27.563, 0.966, 34.344)
+PSH:Upgrade(3)
+
+-- [[ Wave 3 ]] --
+PSH:Place("Soldier", 24.460, 0.980, 34.282)
+PSH:Upgrade(4)
+
+-- [[ Wave 4 ]] --
+PSH:Place("Soldier", 25.944, 0.966, 36.975)
+PSH:Upgrade(5)
+PSH:Place("Soldier", 25.611, 0.987, 26.138)
+
+-- [[ Wave 5 ]] --
+PSH:Upgrade(6)
+PSH:Upgrade(6)
+PSH:Upgrade(5)
+
+-- [[ Wave 6 ]] --
+PSH:Upgrade(1)
+PSH:Upgrade(2)
+
+-- [[ Wave 7 ]] --
+PSH:Upgrade(3)
+PSH:Upgrade(4)
+
+-- [[ Wave 8 ]] --
+PSH:Place("Shotgunner", 36.940, 0.978, 34.188)
+
+-- [[ Wave 9 ]] --
+PSH:Place("Shotgunner", 34.921, 0.975, 34.102)
+PSH:Upgrade(7)
+PSH:Upgrade(8)
+
+-- [[ Wave 10 ]] --
+PSH:Place("Shotgunner", 37.561, 0.995, 28.566)
+PSH:Place("Shotgunner", 35.483, 0.992, 28.569)
+PSH:Upgrade(10)
+
+-- [[ Wave 11 ]] --
+PSH:Upgrade(9)
+PSH:Upgrade(10)
+
+-- [[ Wave 12 ]] --
+PSH:Upgrade(8)
+PSH:Upgrade(7)
+PSH:Upgrade(9)
+
+-- [[ Wave 13 ]] --
+PSH:Place("Soldier", 23.425, 0.973, 38.634)
+PSH:Upgrade(11)
+PSH:Place("Soldier", 26.224, 0.933, 40.318)
+PSH:Upgrade(12)
+PSH:Place("Soldier", 28.790, 0.879, 38.686)
+PSH:Upgrade(13)
+PSH:Upgrade(13)
+
+-- [[ Wave 14 ]] --
+PSH:Upgrade(11)
+PSH:Upgrade(12)
+PSH:Upgrade(11)
+PSH:Upgrade(5)
+
+-- [[ Wave 15 ]] --
+PSH:Upgrade(4)
+PSH:Upgrade(3)
+PSH:Upgrade(13)
+
+-- [[ Wave 16 ]] --
+PSH:Upgrade(2)
+PSH:Upgrade(1)
+PSH:Upgrade(6)
+
+-- [[ Wave 17 ]] --
+PSH:Upgrade(12)
+PSH:Place("Shotgunner", 32.845, 0.971, 34.112)
+PSH:Place("Shotgunner", 30.796, 0.968, 34.142)
+PSH:Place("Shotgunner", 33.478, 0.988, 28.609)
+PSH:Place("Shotgunner", 31.428, 0.984, 28.574)
+PSH:Place("Shotgunner", 32.347, 0.991, 26.589)
+
+-- [[ Wave 18 ]] --
+PSH:Upgrade(16)
+PSH:Upgrade(16)
+PSH:Upgrade(17)
+PSH:Upgrade(18)
+PSH:Upgrade(15)
+PSH:Upgrade(14)
+PSH:Upgrade(17)
+PSH:Upgrade(18)
+PSH:Upgrade(15)
+PSH:Upgrade(14)
+PSH:Upgrade(15)
+
+-- [[ Wave 19 ]] --
+PSH:Upgrade(17)
+PSH:Upgrade(18)
+
+-- [[ Wave 20 ]] --
+PSH:Place("Soldier", 11.525, 0.973, 24.711)
+PSH:Place("Soldier", 14.566, 0.977, 24.715)
+PSH:Place("Soldier", 14.579, 0.979, 27.916)
+PSH:Place("Soldier", 11.135, 0.975, 27.853)
+PSH:Place("Soldier", 10.765, 0.977, 30.928)
+PSH:Place("Soldier", 14.062, 0.981, 30.914)
+PSH:Place("Soldier", 8.135, 0.970, 27.201)
+PSH:Place("Soldier", 8.323, 0.968, 23.878)
+PSH:Place("Soldier", 9.069, 0.966, 20.186)
+PSH:Upgrade(27)
+PSH:Upgrade(27)
+PSH:Upgrade(26)
+PSH:Upgrade(26)
+PSH:Upgrade(20)
+PSH:Upgrade(19)
+PSH:Upgrade(21)
+PSH:Upgrade(22)
+PSH:Upgrade(25)
+PSH:Upgrade(23)
+PSH:Upgrade(24)
+
+-- [[ Wave 21 ]] --
+PSH:Upgrade(24)
+PSH:Upgrade(23)
+PSH:Upgrade(22)
+PSH:Upgrade(21)
+PSH:Upgrade(20)
+PSH:Upgrade(19)
+PSH:Upgrade(25)
+PSH:Upgrade(27)
+PSH:Upgrade(26)
+PSH:Upgrade(19)
+PSH:Upgrade(25)
+PSH:Upgrade(22)
+PSH:Upgrade(21)
+PSH:Upgrade(24)
+PSH:Upgrade(23)
+PSH:Place("Soldier", 22.459, 0.988, 25.739)
+PSH:Upgrade(28)
+PSH:Upgrade(28)
+PSH:Upgrade(28)
+PSH:Place("Soldier", 20.953, 0.988, 28.508)
+PSH:Upgrade(29)
+PSH:Upgrade(29)
+PSH:Upgrade(29)
+
+-- [[ Wave 22 ]] --
+PSH:Place("Soldier", 19.162, 0.984, 25.905)
+PSH:Upgrade(30)
+PSH:Upgrade(30)
+PSH:Upgrade(30)
+PSH:Place("Soldier", 28.167, 0.988, 23.933)
+PSH:Upgrade(31)
+PSH:Upgrade(31)
+PSH:Upgrade(31)
+PSH:Upgrade(16)
+PSH:Upgrade(10)
+PSH:Upgrade(9)
+
+-- [[ Wave 23 ]] --
+PSH:Upgrade(7)
+PSH:Upgrade(8)
+PSH:Upgrade(14)
+PSH:Upgrade(21)
+PSH:Upgrade(20)
+
+-- [[ Wave 24 ]] --
+PSH:Upgrade(24)
+PSH:Upgrade(30)
+PSH:Upgrade(19)
+PSH:Upgrade(22)
+PSH:Upgrade(23)
+PSH:Upgrade(27)
+PSH:Upgrade(26)
+PSH:Upgrade(25)
+
+-- [[ Wave 25 ]] --
+PSH:Upgrade(29)
+PSH:Upgrade(28)
+PSH:Upgrade(1)
+PSH:Upgrade(6)
+PSH:Upgrade(2)
+PSH:Upgrade(31)
+
+-- [[ Wave 26 ]] --
+PSH:Upgrade(4)
+PSH:Upgrade(3)
+PSH:Upgrade(5)
+PSH:Upgrade(11)
+PSH:Upgrade(12)
+PSH:Upgrade(13)
+PSH:Upgrade(20)
+
+-- [[ Wave 27 ]] --
+PSH:Upgrade(17)
+PSH:Upgrade(15)
+PSH:Upgrade(18)
+
+-- [[ Wave 28 ]] --
+PSH:Upgrade(16)
+PSH:Upgrade(10)
+PSH:Upgrade(8)
+
+-- [[ Wave 29 ]] --
+PSH:Upgrade(9)
+PSH:Upgrade(7)
+PSH:Upgrade(14)
+PSH:Place("Soldier", 17.772, 0.987, 33.436)
+PSH:Upgrade(32)
+PSH:Upgrade(32)
+PSH:Upgrade(32)
+PSH:Upgrade(32)
+PSH:Place("Soldier", 17.969, 0.986, 30.340)
+PSH:Upgrade(33)
+PSH:Upgrade(33)
+PSH:Upgrade(33)
+PSH:Upgrade(33)
+
+-- [[ Wave 30 ]] --
+PSH:Place("Soldier", 12.263, 0.996, 33.809)
+PSH:Upgrade(34)
+PSH:Upgrade(34)
+PSH:Upgrade(34)
+PSH:Place("Soldier", 8.988, 0.986, 33.831)
+PSH:Upgrade(35)
+PSH:Upgrade(35)
+PSH:Place("Soldier", 7.672, 0.972, 30.608)
+PSH:Place("Soldier", 5.817, 0.972, 33.556)
+PSH:Place("Soldier", 3.282, 0.967, 31.944)
+PSH:Place("Soldier", 4.882, 0.967, 29.182)
+PSH:Place("Soldier", 3.059, 0.963, 26.550)
+PSH:Upgrade(40)
+PSH:Upgrade(40)
+PSH:Upgrade(40)
+PSH:Upgrade(40)
+PSH:Upgrade(39)
+PSH:Upgrade(39)
+PSH:Upgrade(38)
+PSH:Upgrade(38)
+PSH:Upgrade(38)
+PSH:Upgrade(37)
+PSH:Upgrade(37)
+PSH:Upgrade(37)
+PSH:Upgrade(36)
+PSH:Upgrade(36)
+PSH:Upgrade(36)
+PSH:Upgrade(36)
+PSH:Upgrade(37)
+PSH:Upgrade(39)
+PSH:Upgrade(39)
+PSH:Upgrade(38)
+PSH:Sell(15)
+PSH:Sell(14)
+PSH:Sell(8)
+PSH:Sell(7)
+PSH:Sell(17)
+PSH:Sell(16)
+PSH:Sell(18)
+PSH:Sell(10)
+PSH:Sell(9)
+PSH:Place("Shotgunner", -2.072, 0.953, 23.794)
+PSH:Place("Shotgunner", -2.521, 0.954, 25.762)
+PSH:Place("Shotgunner", -2.214, 0.951, 21.134)
+PSH:Place("Shotgunner", -3.787, 0.950, 22.509)
+PSH:Place("Shotgunner", -4.240, 0.951, 24.621)
+PSH:Upgrade(45)
+PSH:Upgrade(45)
+PSH:Upgrade(45)
+PSH:Upgrade(45)
+PSH:Upgrade(42)
+PSH:Upgrade(42)
+PSH:Upgrade(42)
+PSH:Upgrade(42)
+PSH:Upgrade(41)
+PSH:Upgrade(41)
+PSH:Upgrade(41)
+PSH:Upgrade(41)
+PSH:Upgrade(44)
+PSH:Upgrade(44)
+PSH:Upgrade(44)
+PSH:Upgrade(44)
+PSH:Upgrade(43)
+PSH:Upgrade(43)
+PSH:Upgrade(43)
+PSH:Upgrade(43)
+PSH:Place("Shotgunner", 3.344, 0.957, 19.358)
+PSH:Place("Shotgunner", 3.284, 0.959, 21.609)
+PSH:Place("Shotgunner", 5.450, 0.962, 20.986)
+PSH:Place("Shotgunner", 5.216, 0.963, 23.681)
+PSH:Upgrade(49)
+PSH:Upgrade(49)
+PSH:Upgrade(49)
+PSH:Upgrade(49)
+PSH:Upgrade(47)
+PSH:Upgrade(47)
+PSH:Upgrade(47)
+PSH:Upgrade(47)
+PSH:Upgrade(48)
+PSH:Upgrade(48)
+PSH:Upgrade(48)
+PSH:Upgrade(48)
+PSH:Upgrade(46)
+PSH:Upgrade(46)
+PSH:Upgrade(46)
+PSH:Upgrade(46)
