a24e6337db7f2841f728f26d5b3a3741c0b0fda5
Pick <90311538+8zj@users.noreply.github.com>
2026-09-09 15:16:04 -0400

    Create U-Turn17m.lua

---
+++ Molten/U-Turn17m.lua
@@ -0,0 +1,324 @@
+-- [[
+-- Towers: Minigunner, DJ Booth, Commander, Crook Boss, Brawler
+-- Mode: Molten
+-- GameInfo: Unknown
+-- Map: U-Turn
+-- Modifiers: None
+-- ]]
+
+-- [[ Wave 1 ]] --
+PSH:Place("Brawler", 15.739, 1.000, 18.168)
+PSH:Place("Brawler", 15.373, 1.000, 15.951)
+
+-- [[ Wave 2 ]] --
+PSH:Place("Brawler", 20.973, 1.000, 17.941)
+PSH:Place("Brawler", 21.006, 1.000, 15.883)
+PSH:Upgrade(4)
+
+-- [[ Wave 3 ]] --
+PSH:Upgrade(2)
+PSH:Upgrade(1)
+PSH:Upgrade(3)
+
+-- [[ Wave 5 ]] --
+PSH:Place("Brawler", 15.532, 1.000, 13.834)
+PSH:Upgrade(5)
+
+-- [[ Wave 6 ]] --
+PSH:Place("Minigunner", 15.382, 1.000, 1.908)
+PSH:Upgrade(6)
+
+-- [[ Wave 8 ]] --
+PSH:Upgrade(6)
+PSH:Place("Brawler", 21.185, 1.000, 13.702)
+PSH:Upgrade(7)
+
+-- [[ Wave 9 ]] --
+PSH:Place("Minigunner", 15.657, 1.000, -1.221)
+PSH:Upgrade(8)
+
+-- [[ Wave 10 ]] --
+PSH:Upgrade(8)
+
+-- [[ Wave 11 ]] --
+PSH:Place("Brawler", 15.337, 1.000, 11.766)
+PSH:Upgrade(9)
+PSH:Place("Brawler", 20.881, 1.000, 20.704)
+PSH:Upgrade(10)
+PSH:Place("Brawler", 14.798, 1.000, 21.886)
+PSH:Upgrade(11)
+PSH:Upgrade(11)
+
+-- [[ Wave 12 ]] --
+PSH:Upgrade(10)
+PSH:Upgrade(3)
+
+-- [[ Wave 14 ]] --
+PSH:Upgrade(1)
+
+-- [[ Wave 15 ]] --
+PSH:Place("Minigunner", 15.678, 1.000, -4.457)
+PSH:Place("Minigunner", 13.122, 1.000, -2.862)
+PSH:Place("Minigunner", 12.798, 1.000, 0.188)
+PSH:Upgrade(14)
+PSH:Upgrade(13)
+PSH:Upgrade(12)
+
+-- [[ Wave 16 ]] --
+PSH:Place("DJ Booth", 9.184, 1.000, -1.842)
+PSH:Upgrade(15)
+PSH:Upgrade(15)
+PSH:Upgrade(14)
+PSH:Upgrade(13)
+PSH:Upgrade(12)
+
+-- [[ Wave 17 ]] --
+PSH:Place("Minigunner", 12.547, 1.000, 3.282)
+PSH:Upgrade(16)
+PSH:Upgrade(16)
+PSH:Place("Minigunner", 9.473, 1.000, 2.263)
+PSH:Upgrade(17)
+PSH:Upgrade(17)
+
+-- [[ Wave 18 ]] --
+PSH:Upgrade(9)
+PSH:Upgrade(5)
+PSH:Upgrade(2)
+PSH:Upgrade(7)
+PSH:Upgrade(4)
+
+-- [[ Wave 19 ]] --
+PSH:Upgrade(9)
+PSH:Upgrade(7)
+PSH:Upgrade(5)
+PSH:Upgrade(4)
+PSH:Upgrade(2)
+
+-- [[ Wave 20 ]] --
+PSH:Upgrade(1)
+PSH:Upgrade(3)
+
+-- [[ Wave 21 ]] --
+PSH:Upgrade(10)
+PSH:Upgrade(11)
+PSH:Upgrade(12)
+PSH:Upgrade(15)
+
+-- [[ Wave 22 ]] --
+PSH:Upgrade(15)
+
+-- [[ Wave 23 ]] --
+PSH:Upgrade(17)
+PSH:Upgrade(14)
+PSH:Upgrade(13)
+
+-- [[ Wave 24 ]] --
+PSH:Upgrade(6)
+PSH:Upgrade(8)
+
+-- [[ Wave 25 ]] --
+PSH:Upgrade(9)
+PSH:Upgrade(5)
+PSH:Upgrade(7)
+PSH:Upgrade(4)
+PSH:Place("Crook Boss", 9.633, 1.000, 5.523)
+PSH:Upgrade(18)
+PSH:Upgrade(18)
+
+-- [[ Wave 26 ]] --
+PSH:Upgrade(2)
+PSH:Upgrade(1)
+PSH:Upgrade(3)
+PSH:Upgrade(10)
+PSH:Upgrade(11)
+PSH:Place("Commander", 15.458, 1.000, 4.938)
+PSH:Place("Commander", 12.693, 1.000, 6.422)
+PSH:Place("Commander", 15.338, 1.000, 8.106)
+PSH:Upgrade(21)
+PSH:Upgrade(21)
+PSH:Upgrade(20)
+PSH:Upgrade(20)
+PSH:Upgrade(19)
+PSH:Upgrade(19)
+PSH:Upgrade(21)
+PSH:Upgrade(19)
+PSH:Upgrade(20)
+
+-- [[ Wave 27 ]] --
+PSH:Place("Minigunner", 9.064, 1.000, -5.943)
+PSH:Place("Minigunner", 12.277, 1.000, -5.922)
+PSH:Place("Minigunner", 15.427, 1.000, -7.495)
+PSH:Place("Minigunner", 15.037, 1.000, -10.535)
+PSH:Place("Commander", 12.341, 1.000, -9.114)
+PSH:Place("Minigunner", 9.329, 1.000, -9.043)
+PSH:Upgrade(27)
+PSH:Upgrade(27)
+PSH:Upgrade(27)
+PSH:Upgrade(22)
+PSH:Upgrade(22)
+PSH:Upgrade(22)
+PSH:Upgrade(23)
+
+-- [[ Wave 28 ]] --
+PSH:Upgrade(23)
+PSH:Upgrade(24)
+PSH:Upgrade(25)
+PSH:Upgrade(25)
+PSH:Upgrade(24)
+PSH:Upgrade(24)
+PSH:Upgrade(25)
+PSH:Upgrade(23)
+
+-- [[ Wave 29 ]] --
+PSH:Upgrade(15)
+PSH:Upgrade(9)
+PSH:Upgrade(5)
+PSH:Upgrade(7)
+PSH:Upgrade(4)
+PSH:Upgrade(2)
+
+-- [[ Wave 30 ]] --
+PSH:Upgrade(1)
+PSH:Upgrade(11)
+PSH:Upgrade(3)
+PSH:Upgrade(10)
+PSH:Place("Crook Boss", 9.318, 1.000, -12.178)
+PSH:Upgrade(28)
+PSH:Upgrade(28)
+PSH:Upgrade(26)
+PSH:Upgrade(26)
+PSH:Upgrade(26)
+PSH:Place("Crook Boss", 3.126, 1.000, -9.805)
+PSH:Upgrade(29)
+PSH:Upgrade(29)
+PSH:Place("Crook Boss", 3.151, 1.000, -6.757)
+PSH:Upgrade(30)
+PSH:Upgrade(30)
+PSH:Place("Crook Boss", 10.488, 1.000, 8.768)
+PSH:Upgrade(31)
+PSH:Upgrade(31)
+PSH:Upgrade(18)
+
+-- [[ Wave 31 ]] --
+PSH:Upgrade(31)
+PSH:Upgrade(28)
+PSH:Upgrade(30)
+PSH:Upgrade(29)
+PSH:Upgrade(22)
+PSH:Upgrade(23)
+
+-- [[ Wave 32 ]] --
+PSH:Upgrade(13)
+PSH:Upgrade(14)
+PSH:Upgrade(17)
+PSH:Upgrade(16)
+PSH:Upgrade(16)
+
+-- [[ Wave 33 ]] --
+PSH:Upgrade(27)
+PSH:Upgrade(8)
+PSH:Upgrade(24)
+PSH:Upgrade(25)
+PSH:Upgrade(6)
+PSH:Upgrade(20)
+PSH:Upgrade(19)
+PSH:Upgrade(21)
+
+-- [[ Wave 34 ]] --
+PSH:Upgrade(12)
+PSH:Upgrade(26)
+PSH:Place("Minigunner", 3.231, 1.000, -3.710)
+PSH:Upgrade(32)
+PSH:Upgrade(32)
+PSH:Upgrade(32)
+PSH:Upgrade(32)
+PSH:Place("Minigunner", 3.074, 1.000, -0.608)
+PSH:Place("Minigunner", 3.037, 1.000, 2.550)
+PSH:Place("Minigunner", 3.623, 1.000, 6.332)
+PSH:Upgrade(35)
+PSH:Upgrade(35)
+PSH:Upgrade(35)
+PSH:Upgrade(35)
+PSH:Upgrade(34)
+PSH:Upgrade(34)
+PSH:Upgrade(34)
+PSH:Upgrade(34)
+PSH:Upgrade(33)
+PSH:Upgrade(33)
+PSH:Upgrade(33)
+PSH:Upgrade(33)
+PSH:Upgrade(18)
+
+-- [[ Wave 35 ]] --
+PSH:Upgrade(31)
+PSH:Upgrade(28)
+PSH:Upgrade(29)
+PSH:Sell(9)
+PSH:Sell(5)
+PSH:Sell(2)
+PSH:Sell(1)
+PSH:Sell(3)
+PSH:Sell(7)
+PSH:Sell(4)
+PSH:Sell(11)
+PSH:Sell(10)
+PSH:Place("Brawler", 10.872, 1.000, -17.687)
+PSH:Upgrade(36)
+PSH:Upgrade(36)
+PSH:Upgrade(36)
+PSH:Upgrade(36)
+PSH:Upgrade(36)
+PSH:Place("Brawler", 13.448, 1.000, -18.127)
+PSH:Upgrade(37)
+PSH:Upgrade(37)
+PSH:Upgrade(37)
+PSH:Upgrade(37)
+PSH:Upgrade(37)
+PSH:Place("Brawler", 8.935, 1.000, -15.667)
+PSH:Upgrade(38)
+PSH:Upgrade(38)
+PSH:Upgrade(38)
+PSH:Upgrade(38)
+PSH:Upgrade(38)
+PSH:Place("Brawler", 5.200, 1.000, -21.737)
+PSH:Upgrade(39)
+PSH:Upgrade(39)
+PSH:Upgrade(39)
+PSH:Upgrade(39)
+PSH:Upgrade(39)
+PSH:Place("Brawler", 8.005, 1.000, -23.408)
+PSH:Upgrade(40)
+PSH:Upgrade(40)
+PSH:Upgrade(40)
+PSH:Upgrade(40)
+PSH:Upgrade(40)
+PSH:Place("Brawler", 11.759, 1.000, -23.638)
+PSH:Upgrade(41)
+PSH:Upgrade(41)
+PSH:Upgrade(41)
+PSH:Upgrade(41)
+PSH:Upgrade(41)
+PSH:Place("Brawler", 3.179, 1.000, -19.051)
+PSH:Upgrade(42)
+PSH:Upgrade(42)
+PSH:Upgrade(42)
+PSH:Upgrade(42)
+PSH:Upgrade(42)
+PSH:Place("Brawler", 3.787, 1.000, -16.567)
+PSH:Upgrade(43)
+PSH:Upgrade(43)
+PSH:Upgrade(43)
+PSH:Upgrade(43)
+PSH:Upgrade(43)
+PSH:Place("Brawler", 2.937, 1.000, -13.533)
+PSH:Upgrade(44)
+PSH:Upgrade(44)
+PSH:Upgrade(44)
+PSH:Upgrade(44)
+PSH:Upgrade(44)
+PSH:Place("Brawler", 0.822, 1.000, -8.422)
+PSH:Upgrade(45)
+PSH:Upgrade(45)
+PSH:Upgrade(45)
+PSH:Upgrade(45)
+PSH:Upgrade(45)
