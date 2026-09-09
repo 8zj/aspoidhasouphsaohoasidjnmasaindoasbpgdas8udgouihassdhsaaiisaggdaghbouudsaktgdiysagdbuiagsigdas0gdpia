81390d77e53238d43dbac7d697a875832561a737
Pick <90311538+8zj@users.noreply.github.com>
2026-09-09 01:03:44 -0400

    Update Losingstrat1.lua

--- Hardcore/Losingstrat1.lua
+++ Hardcore/Losingstrat1.lua
@@ -1,1 +1,47 @@
+-- [[
+-- Towers: Shotgunner, Pyromancer, None, None, None
+-- Mode: Easy
+-- GameInfo: Unknown
+-- Map: Wretched Front
+-- Modifiers: None
+-- ]]
 
+-- [[ Wave 1 ]] --
+PSH:Place("Pyromancer", -9.651, 0.992, -31.867)
+PSH:Upgrade(1)
+
+-- [[ Wave 3 ]] --
+PSH:Place("Pyromancer", -9.778, 0.960, -28.658)
+
+-- [[ Wave 4 ]] --
+PSH:Upgrade(2)
+
+-- [[ Wave 6 ]] --
+PSH:Place("Pyromancer", -7.060, 0.947, -30.119)
+PSH:Upgrade(3)
+
+-- [[ Wave 7 ]] --
+PSH:Upgrade(3)
+
+-- [[ Wave 8 ]] --
+PSH:Upgrade(2)
+
+-- [[ Wave 9 ]] --
+PSH:Upgrade(1)
+
+-- [[ Wave 10 ]] --
+PSH:Place("Pyromancer", -3.961, 0.965, -29.798)
+PSH:Upgrade(4)
+
+-- [[ Wave 11 ]] --
+PSH:Upgrade(4)
+
+-- [[ Wave 12 ]] --
+PSH:Place("Shotgunner", -5.415, 0.965, -32.406)
+
+-- [[ Wave 13 ]] --
+PSH:Upgrade(5)
+
+-- [[ Wave 14 ]] --
+PSH:Place("Shotgunner", -5.590, 0.987, -34.856)
+PSH:Upgrade(6)
