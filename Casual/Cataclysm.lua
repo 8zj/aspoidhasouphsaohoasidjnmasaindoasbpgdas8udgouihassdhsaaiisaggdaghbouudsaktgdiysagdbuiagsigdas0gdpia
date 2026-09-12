d3d9d77e9b22c106bfb04d4488b7afecfd4a45b8
qxDawidxp <132229657+qxDawidxp@users.noreply.github.com>
2026-09-12 22:35:32 +0200

    Add soldier placement and upgrade logic for Cataclysm
    This file contains the setup and upgrade sequence for soldiers in the Casual mode of the Cataclysm map, detailing actions for 28 waves.

---
+++ Casual/Cataclysm.lua
@@ -0,0 +1,197 @@
+-- [[
+-- Towers: Soldier, None, None, None, None
+-- Mode: Casual
+-- GameInfo: Unknown
+-- Map: Cataclysm
+-- Modifiers: None
+-- ]]
+
+-- [[ Wave 1 ]] --
+PSH:Place("Soldier", 2.685, 1.000, 29.663)
+PSH:Upgrade(1)
+
+-- [[ Wave 2 ]] --
+PSH:Place("Soldier", -0.342, 1.000, 29.834)
+PSH:Upgrade(2)
+
+-- [[ Wave 3 ]] --
+PSH:Place("Soldier", -3.352, 1.000, 29.912)
+PSH:Upgrade(3)
+
+-- [[ Wave 4 ]] --
+PSH:Place("Soldier", -6.400, 1.000, 29.783)
+PSH:Upgrade(4)
+PSH:Place("Soldier", -9.500, 1.000, 29.783)
+PSH:Upgrade(5)
+
+-- [[ Wave 5 ]] --
+PSH:Upgrade(1)
+PSH:Upgrade(2)
+PSH:Upgrade(3)
+
+-- [[ Wave 6 ]] --
+PSH:Upgrade(4)
+
+-- [[ Wave 7 ]] --
+PSH:Upgrade(5)
+PSH:Place("Soldier", 2.533, 1.000, 26.631)
+PSH:Upgrade(6)
+
+-- [[ Wave 8 ]] --
+PSH:Place("Soldier", -0.584, 1.000, 26.720)
+PSH:Upgrade(7)
+PSH:Place("Soldier", -3.663, 1.000, 26.818)
+PSH:Upgrade(8)
+PSH:Place("Soldier", -6.672, 1.000, 26.786)
+
+-- [[ Wave 9 ]] --
+PSH:Upgrade(9)
+PSH:Place("Soldier", -9.767, 1.000, 26.719)
+PSH:Upgrade(10)
+PSH:Upgrade(10)
+PSH:Upgrade(9)
+PSH:Upgrade(8)
+PSH:Upgrade(7)
+PSH:Upgrade(6)
+
+-- [[ Wave 10 ]] --
+PSH:Upgrade(1)
+PSH:Upgrade(2)
+
+-- [[ Wave 11 ]] --
+PSH:Upgrade(6)
+PSH:Upgrade(7)
+
+-- [[ Wave 12 ]] --
+PSH:Upgrade(3)
+PSH:Upgrade(8)
+
+-- [[ Wave 13 ]] --
+PSH:Upgrade(4)
+PSH:Upgrade(9)
+
+-- [[ Wave 14 ]] --
+PSH:Upgrade(5)
+PSH:Upgrade(10)
+
+-- [[ Wave 15 ]] --
+PSH:Upgrade(1)
+
+-- [[ Wave 16 ]] --
+PSH:Upgrade(2)
+
+-- [[ Wave 17 ]] --
+PSH:Upgrade(6)
+PSH:Upgrade(7)
+
+-- [[ Wave 18 ]] --
+PSH:Upgrade(3)
+
+-- [[ Wave 19 ]] --
+PSH:Upgrade(8)
+PSH:Upgrade(4)
+
+-- [[ Wave 20 ]] --
+PSH:Upgrade(9)
+PSH:Upgrade(5)
+
+-- [[ Wave 21 ]] --
+PSH:Upgrade(10)
+PSH:Place("Soldier", -10.536, 1.000, 23.733)
+PSH:Upgrade(11)
+PSH:Upgrade(11)
+PSH:Upgrade(11)
+PSH:Upgrade(11)
+
+-- [[ Wave 22 ]] --
+PSH:Place("Soldier", -7.482, 1.000, 23.767)
+PSH:Upgrade(12)
+PSH:Upgrade(12)
+PSH:Upgrade(12)
+PSH:Upgrade(12)
+PSH:Place("Soldier", -4.414, 1.000, 23.724)
+PSH:Upgrade(13)
+PSH:Upgrade(13)
+PSH:Upgrade(13)
+PSH:Upgrade(13)
+PSH:Place("Soldier", -1.346, 1.000, 23.734)
+PSH:Upgrade(14)
+PSH:Upgrade(14)
+PSH:Upgrade(14)
+PSH:Upgrade(14)
+
+-- [[ Wave 23 ]] --
+PSH:Place("Soldier", 1.660, 1.000, 23.682)
+PSH:Upgrade(15)
+PSH:Upgrade(15)
+PSH:Upgrade(15)
+PSH:Upgrade(15)
+PSH:Place("Soldier", -10.331, 1.000, 20.526)
+PSH:Upgrade(16)
+PSH:Upgrade(16)
+PSH:Upgrade(16)
+PSH:Upgrade(16)
+
+-- [[ Wave 24 ]] --
+PSH:Place("Soldier", -7.294, 1.000, 20.741)
+PSH:Upgrade(17)
+PSH:Upgrade(17)
+PSH:Upgrade(17)
+PSH:Upgrade(17)
+PSH:Place("Soldier", -4.243, 1.000, 20.578)
+PSH:Upgrade(18)
+PSH:Upgrade(18)
+PSH:Upgrade(18)
+PSH:Upgrade(18)
+PSH:Place("Soldier", -1.175, 1.000, 20.586)
+PSH:Upgrade(19)
+PSH:Upgrade(19)
+PSH:Upgrade(19)
+PSH:Upgrade(19)
+PSH:Place("Soldier", 2.001, 1.000, 20.515)
+
+-- [[ Wave 25 ]] --
+PSH:Upgrade(20)
+PSH:Upgrade(20)
+PSH:Upgrade(20)
+PSH:Upgrade(20)
+PSH:Place("Soldier", -3.304, 1.000, 35.199)
+PSH:Upgrade(21)
+PSH:Upgrade(21)
+PSH:Upgrade(21)
+PSH:Upgrade(21)
+PSH:Place("Soldier", -6.513, 1.000, 35.044)
+PSH:Upgrade(22)
+PSH:Upgrade(22)
+PSH:Upgrade(22)
+PSH:Upgrade(22)
+PSH:Place("Soldier", -9.836, 1.000, 35.332)
+PSH:Upgrade(23)
+PSH:Upgrade(23)
+PSH:Upgrade(23)
+PSH:Upgrade(23)
+PSH:Place("Soldier", -12.905, 1.000, 35.206)
+PSH:Upgrade(24)
+PSH:Upgrade(24)
+PSH:Upgrade(24)
+PSH:Upgrade(24)
+PSH:Place("Soldier", -16.650, 1.000, 31.632)
+PSH:Upgrade(25)
+PSH:Upgrade(25)
+PSH:Upgrade(25)
+PSH:Upgrade(25)
+PSH:Place("Soldier", -16.249, 1.000, 28.294)
+PSH:Upgrade(26)
+PSH:Upgrade(26)
+PSH:Upgrade(26)
+PSH:Upgrade(26)
+PSH:Place("Soldier", -16.078, 1.000, 24.537)
+PSH:Upgrade(27)
+PSH:Upgrade(27)
+PSH:Upgrade(27)
+PSH:Upgrade(27)
+PSH:Place("Soldier", -15.894, 1.000, 21.512)
+PSH:Upgrade(28)
+PSH:Upgrade(28)
+PSH:Upgrade(28)
+PSH:Upgrade(28)
