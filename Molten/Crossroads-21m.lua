f07c8fa9b7f0a21f33af9a762d8d731118bc621f
Pick <90311538+8zj@users.noreply.github.com>
2026-09-21 11:46:25 -0400

    Create Crossroads-21m.lua

---
+++ Molten/Crossroads-21m.lua
@@ -0,0 +1,292 @@
+-- [[
+-- Towers: Minigunner, DJ Booth, Commander, Crook Boss, Brawler*
+-- Mode: Molten*
+-- GameInfo: Unknown*
+-- Map: Crossroads*
+-- Modifiers: None*
+-- ]]
+-- [[ Wave 1 ]] --
+PSH:Place("Brawler", -2.594, 1.000, -38.136)
+PSH:Upgrade(1)
+PSH:Place("Brawler", 2.874, 1.000, -37.553)
+
+-- [[ Wave 2 ]] --
+PSH:Upgrade(2)
+PSH:Place("Brawler", -2.645, 1.000, -36.073)
+
+-- [[ Wave 3 ]] --
+PSH:Upgrade(3)
+PSH:Place("Brawler", 2.691, 1.000, -35.427)
+PSH:Upgrade(4)
+
+-- [[ Wave 4 ]] --
+PSH:Place("Brawler", 2.648, 1.000, -33.410)
+PSH:Upgrade(5)
+
+-- [[ Wave 5 ]] --
+PSH:Place("Brawler", -2.749, 1.000, -33.846)
+PSH:Upgrade(6)
+
+-- [[ Wave 6 ]] --
+PSH:Place("Brawler", 2.680, 1.000, -31.285)
+PSH:Upgrade(7)
+PSH:Place("Brawler", -2.868, 1.000, -31.659)
+PSH:Upgrade(8)
+
+-- [[ Wave 7 ]] --
+PSH:Sell(7)
+PSH:Sell(8)
+PSH:Sell(6)
+PSH:Sell(5)
+PSH:Place("Minigunner", 9.116, 1.000, 8.193)
+PSH:Upgrade(4)
+
+-- [[ Wave 8 ]] --
+PSH:Upgrade(3)
+
+-- [[ Wave 9 ]] --
+PSH:Place("Brawler", -2.671, 1.000, -33.549)
+PSH:Upgrade(10)
+PSH:Place("Brawler", 2.801, 1.000, -33.204)
+PSH:Upgrade(11)
+
+-- [[ Wave 10 ]] --
+PSH:Upgrade(9)
+PSH:Place("Minigunner", 6.044, 1.000, 8.381)
+PSH:Upgrade(12)
+
+-- [[ Wave 11 ]] --
+PSH:Upgrade(11)
+
+-- [[ Wave 12 ]] --
+PSH:Upgrade(2)
+PSH:Place("Brawler", -2.556, 1.000, -31.443)
+PSH:Upgrade(13)
+PSH:Place("Brawler", 2.693, 1.000, -31.095)
+PSH:Upgrade(14)
+
+-- [[ Wave 13 ]] --
+PSH:Upgrade(14)
+PSH:Upgrade(9)
+PSH:Upgrade(12)
+
+-- [[ Wave 14 ]] --
+PSH:Upgrade(13)
+PSH:Place("Brawler", -2.761, 1.000, -29.377)
+PSH:Upgrade(15)
+PSH:Place("Brawler", 2.662, 1.000, -29.092)
+
+-- [[ Wave 15 ]] --
+PSH:Upgrade(16)
+PSH:Upgrade(16)
+PSH:Upgrade(15)
+PSH:Place("Minigunner", 7.754, 1.000, 10.927)
+PSH:Upgrade(17)
+
+-- [[ Wave 16 ]] --
+PSH:Upgrade(17)
+PSH:Place("Minigunner", 7.463, 1.000, 5.583)
+PSH:Upgrade(18)
+PSH:Upgrade(18)
+
+-- [[ Wave 17 ]] --
+PSH:Place("Crook Boss", 4.114, 1.000, 5.754)
+PSH:Upgrade(19)
+PSH:Upgrade(19)
+
+-- [[ Wave 18 ]] --
+PSH:Place("DJ Booth", 11.722, 1.000, -2.630)
+PSH:Upgrade(20)
+PSH:Upgrade(20)
+PSH:SetOption(20, "Track", "Green")
+
+-- [[ Wave 19 ]] --
+PSH:Place("Crook Boss", 5.176, 1.000, 12.562)
+PSH:Upgrade(21)
+PSH:Upgrade(21)
+PSH:Upgrade(12)
+PSH:Upgrade(20)
+
+-- [[ Wave 20 ]] --
+PSH:Upgrade(9)
+
+-- [[ Wave 21 ]] --
+PSH:Upgrade(18)
+PSH:Place("Minigunner", 10.923, 1.000, 10.890)
+PSH:Upgrade(22)
+PSH:Upgrade(22)
+PSH:Upgrade(22)
+
+-- [[ Wave 22 ]] --
+PSH:Place("Crook Boss", 4.586, 1.000, 15.623)
+PSH:Upgrade(23)
+PSH:Upgrade(23)
+PSH:Place("Crook Boss", 3.778, 1.000, 2.715)
+PSH:Upgrade(24)
+PSH:Upgrade(24)
+
+-- [[ Wave 23 ]] --
+PSH:Upgrade(20)
+PSH:Place("Minigunner", 7.040, 1.000, 2.605)
+
+-- [[ Wave 24 ]] --
+PSH:Upgrade(25)
+PSH:Upgrade(25)
+PSH:Upgrade(25)
+PSH:Place("Commander", 9.911, 1.000, 3.541)
+PSH:Upgrade(26)
+PSH:Upgrade(26)
+PSH:Place("Commander", 13.136, 1.000, 3.299)
+PSH:Upgrade(27)
+PSH:Place("Commander", 11.525, 1.000, 6.188)
+PSH:Upgrade(28)
+PSH:Upgrade(28)
+PSH:Upgrade(28)
+PSH:Upgrade(26)
+
+-- [[ Wave 25 ]] --
+PSH:Upgrade(27)
+PSH:Upgrade(27)
+PSH:Place("Minigunner", 7.997, 1.000, 14.146)
+PSH:Upgrade(29)
+PSH:Upgrade(29)
+PSH:Place("Minigunner", 13.376, 1.000, 8.627)
+PSH:Upgrade(30)
+PSH:Upgrade(30)
+PSH:Upgrade(30)
+
+-- [[ Wave 26 ]] --
+PSH:Upgrade(16)
+PSH:Upgrade(15)
+PSH:Upgrade(14)
+PSH:Upgrade(13)
+PSH:Upgrade(11)
+PSH:Upgrade(10)
+PSH:Upgrade(10)
+PSH:Upgrade(4)
+PSH:Upgrade(3)
+PSH:Upgrade(2)
+PSH:Upgrade(1)
+PSH:Upgrade(1)
+
+-- [[ Wave 27 ]] --
+PSH:Upgrade(20)
+PSH:Upgrade(21)
+
+-- [[ Wave 28 ]] --
+PSH:Upgrade(24)
+PSH:Upgrade(19)
+PSH:Upgrade(23)
+
+-- [[ Wave 29 ]] --
+PSH:Place("Crook Boss", 2.917, 1.000, -2.774)
+PSH:Upgrade(31)
+PSH:Upgrade(31)
+PSH:Upgrade(31)
+PSH:Upgrade(29)
+PSH:Upgrade(17)
+
+-- [[ Wave 30 ]] --
+PSH:Upgrade(16)
+PSH:Upgrade(15)
+PSH:Upgrade(13)
+PSH:Upgrade(10)
+PSH:Upgrade(14)
+PSH:Upgrade(11)
+PSH:Upgrade(4)
+PSH:Upgrade(2)
+PSH:Upgrade(1)
+PSH:Upgrade(3)
+PSH:SetOption(20, "Track", "Red")
+PSH:Place("Minigunner", 16.181, 1.000, 2.908)
+PSH:Place("Minigunner", 19.320, 1.000, 3.699)
+PSH:Place("Minigunner", 18.934, 1.000, 6.959)
+PSH:Place("Minigunner", 19.454, 1.000, 10.316)
+PSH:Place("Minigunner", 16.420, 1.000, 12.219)
+PSH:Place("Minigunner", 15.887, 1.000, 6.512)
+PSH:Upgrade(37)
+PSH:Upgrade(37)
+PSH:Upgrade(37)
+PSH:Upgrade(32)
+PSH:Upgrade(32)
+PSH:Upgrade(32)
+PSH:Upgrade(33)
+PSH:Upgrade(33)
+PSH:Upgrade(33)
+PSH:Upgrade(34)
+PSH:Upgrade(34)
+PSH:Upgrade(34)
+PSH:Upgrade(35)
+PSH:Upgrade(35)
+PSH:Upgrade(35)
+PSH:Upgrade(36)
+PSH:Upgrade(36)
+
+-- [[ Wave 31 ]] --
+PSH:Upgrade(36)
+PSH:SetOption(20, "Track", "Green")
+PSH:Upgrade(22)
+PSH:Upgrade(9)
+PSH:Upgrade(18)
+PSH:Upgrade(17)
+
+-- [[ Wave 32 ]] --
+PSH:Upgrade(12)
+PSH:Upgrade(28)
+PSH:Upgrade(27)
+PSH:Upgrade(26)
+PSH:SetOption(20, "Track", "Red")
+PSH:SetOption(20, "Track", "Green")
+PSH:SetOption(20, "Track", "Green")
+PSH:SetOption(20, "Track", "Green")
+
+-- [[ Wave 33 ]] --
+PSH:Upgrade(30)
+PSH:Upgrade(37)
+PSH:Upgrade(32)
+PSH:Upgrade(33)
+PSH:Upgrade(34)
+PSH:Upgrade(35)
+PSH:Upgrade(25)
+PSH:Upgrade(29)
+
+-- [[ Wave 34 ]] --
+PSH:Upgrade(36)
+PSH:Place("Minigunner", 11.056, 1.000, 14.195)
+PSH:Upgrade(38)
+PSH:Upgrade(38)
+PSH:Upgrade(38)
+PSH:Upgrade(38)
+PSH:Place("Minigunner", 2.856, 1.000, 18.116)
+PSH:Upgrade(39)
+PSH:Upgrade(39)
+PSH:Upgrade(39)
+PSH:Upgrade(39)
+PSH:Place("Minigunner", 4.862, 1.000, 20.437)
+PSH:Upgrade(40)
+PSH:Upgrade(40)
+PSH:Upgrade(40)
+PSH:Upgrade(40)
+PSH:Place("Minigunner", 6.846, 1.000, 17.872)
+PSH:Upgrade(41)
+PSH:Upgrade(41)
+PSH:Upgrade(41)
+PSH:Upgrade(41)
+PSH:Place("Minigunner", 10.425, 1.000, 17.490)
+PSH:Upgrade(42)
+PSH:Upgrade(42)
+PSH:Upgrade(42)
+PSH:Upgrade(42)
+PSH:Place("Commander", 8.981, 1.000, 20.155)
+PSH:Upgrade(43)
+PSH:Upgrade(43)
+PSH:Upgrade(43)
+PSH:Upgrade(43)
+
+-- [[ Wave 35 ]] --
+PSH:Upgrade(24)
+PSH:Upgrade(19)
+PSH:Upgrade(21)
+PSH:SetOption(20, "Track", "Red")
+PSH:Upgrade(23)
+PSH:Upgrade(31)
