52dfee3c3f47d21167cba3116c66cb1f86452b23
Pick <90311538+8zj@users.noreply.github.com>
2026-09-21 22:52:03 -0400

    Create Easystrat1.lua

---
+++ Strats/Easy/Easystrat1.lua
@@ -0,0 +1,58 @@
+getgenv().PickHubLOL = {
+    Main = {
+        AutoStrat       = true,
+        AutoRestart     = false,
+        AutoReturnLobby = true,
+        AutoSkip        = false,
+        SendWebhook     = false,
+        TimeScale       = 2,
+    },
+    Mis = {
+        AntiLag    = false,
+        AutoPickup = true,
+        ANTIAFK    = true,
+        FreeCam    = false,
+    },
+    Marco = {
+        AutoDJ          = false,
+        Autocommander   = false,
+        APCSpam         = false,
+        AutoMercenary   = false,
+        AutoMilitary    = false,
+        AutoNecromancer = false,
+    },
+    Urls = {
+        Webhook  = "",
+        macroURL = "",
+    },
+    GameInfo = {
+        Enabled   = true,
+        MapIndex  = 1,
+        VIPMap   = "",
+        Mode      = "Easy",
+        Maps      = {
+            ["GrassIsle"] = {
+                Loadout = {"Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/EasyMode/GrassIsle.lua"
+            },
+            ["DustyBridges"] = {
+                Loadout = {"Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/EasyMode/DustyBridges.lua"
+            },
+            ["Crossroads "] = {
+                Loadout = {"Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/EasyMode/CrossRoads.lua"
+            },
+            ["ConstructionCrazy"] = {
+                Loadout = {"Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/EasyMode/ConstructionCrazy.lua"
+            },
+            ["AbyssalTrench"] = {
+                Loadout = {"Scout"},
+                URL     = "https://raw.githubusercontent.com/8zj/aspoidhasouphsaohoasidjnmasaindoasbpgdas8udgouihassdhsaaiisaggdaghbouudsaktgdiysagdbuiagsigdas0gdpia/refs/heads/main/EasyMode/AbyssalTrench.lua"
+            },
+        },
+        Modifiers = {}
+    },
+}
+loadstring(game:HttpGet("http://pickscripthub.xyz/api/execute/tds-multi-map"))()
