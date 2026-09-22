763cc2c19e2d075b4e9007d987e8b3ed2451b7c8
Pick <90311538+8zj@users.noreply.github.com>
2026-09-21 22:49:11 -0400

    Create GrassIsle.lua

---
+++ EasyMode/GrassIsle.lua
@@ -0,0 +1,101 @@
+PSH:Place("Scout", 11.774942, 1.000003, -17.127649)
+PSH:Place("Scout", 11.953791, 1.000003, -20.178905)
+PSH:Place("Scout", 12.280043, 1.000004, -23.210201)
+PSH:Place("Scout", 15.27735, 1.000004, -22.473429)
+PSH:Upgrade(1)
+PSH:Upgrade(2)
+
+-- [ Wave 2 ] --
+PSH:Place("Scout", 14.868414, 1.000003, -19.43437)
+PSH:Upgrade(5)
+PSH:Upgrade(4)
+
+-- [ Wave 3 ] --
+PSH:Upgrade(3)
+PSH:Place("Scout", 14.742072, 1.000003, -16.339964)
+PSH:Upgrade(6)
+PSH:Place("Scout", 17.761395, 1.000003, -20.615192)
+
+-- [ Wave 4 ] --
+PSH:Upgrade(7)
+PSH:Place("Scout", 17.645222, 1.000003, -17.590342)
+PSH:Upgrade(8)
+PSH:Place("Scout", 17.137068, 1.000002, -14.512873)
+PSH:Upgrade(9)
+PSH:Place("Scout", 11.568352, 1.000002, -14.093931)
+PSH:Upgrade(10)
+PSH:Place("Scout", 14.398523, 1.000002, -12.748578)
+PSH:Upgrade(11)
+PSH:Place("Scout", 11.690114, 1.000002, -11.082199)
+PSH:Upgrade(12)
+
+-- [ Wave 5 ] --
+PSH:Place("Scout", 9.260077, 1.000004, -23.540365)
+PSH:Upgrade(13)
+PSH:Upgrade(1)
+PSH:Upgrade(2)
+PSH:Upgrade(3)
+
+-- [ Wave 6 ] --
+PSH:Upgrade(13)
+PSH:Upgrade(10)
+PSH:Upgrade(12)
+PSH:Upgrade(11)
+
+-- [ Wave 7 ] --
+PSH:Upgrade(6)
+
+-- [ Wave 8 ] --
+PSH:Upgrade(5)
+PSH:Upgrade(7)
+PSH:Upgrade(4)
+PSH:Upgrade(8)
+PSH:Upgrade(9)
+
+-- [ Wave 9 ] --
+PSH:Upgrade(1)
+PSH:Upgrade(1)
+
+-- [ Wave 10 ] --
+PSH:Upgrade(6)
+
+-- [ Wave 11 ] --
+PSH:Upgrade(6)
+
+-- [ Wave 12 ] --
+PSH:Upgrade(2)
+PSH:Upgrade(2)
+PSH:Upgrade(13)
+
+-- [ Wave 13 ] --
+PSH:Upgrade(13)
+
+-- [ Wave 14 ] --
+PSH:Upgrade(3)
+PSH:Upgrade(3)
+PSH:Upgrade(4)
+
+-- [ Wave 15 ] --
+PSH:Upgrade(4)
+PSH:Upgrade(5)
+PSH:Upgrade(5)
+
+-- [ Wave 16 ] --
+PSH:Upgrade(7)
+PSH:Upgrade(7)
+PSH:Upgrade(8)
+
+-- [ Wave 17 ] --
+PSH:Upgrade(8)
+PSH:Upgrade(10)
+PSH:Upgrade(10)
+
+-- [ Wave 18 ] --
+PSH:Upgrade(9)
+PSH:Upgrade(9)
+PSH:Upgrade(11)
+PSH:Upgrade(11)
+
+-- [ Wave 19 ] --
+PSH:Upgrade(12)
+PSH:Upgrade(12)
