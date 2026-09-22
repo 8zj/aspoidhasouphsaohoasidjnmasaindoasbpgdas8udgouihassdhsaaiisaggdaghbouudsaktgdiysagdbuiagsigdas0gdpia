fb46469945001a7ff8e80f2da1df6fd18eeffacd
luhhjonxyz <jonhollis262@gmail.com>
2026-09-21 22:39:04 -0400

    Create Four Seasons.lua
    This file contains the setup for the Easy mode of the Four Seasons map, detailing the placement and upgrading of Soldiers and Scouts across multiple waves.

---
+++ EasyMode/Four Seasons.lua
@@ -0,0 +1,144 @@
+*-- [[*
+*-- Towers: Scout, Soldier, None, None, None*
+*-- Mode: Easy*
+*-- GameInfo: Unknown*
+*-- Map: Four Seasons*
+*-- Modifiers: None*
+*-- ]]*
+-- [[ Wave 1 ]] --
+PSH:Place("Soldier", 37.872, 0.750, -3.733)
+PSH:Upgrade(1)
+
+-- [[ Wave 2 ]] --
+PSH:Place("Soldier", 37.520, 0.750, -6.914)
+
+-- [[ Wave 3 ]] --
+PSH:Place("Soldier", 35.018, 0.750, -2.539)
+PSH:Place("Soldier", 42.203, 0.750, -7.780)
+
+-- [[ Wave 4 ]] --
+PSH:Place("Soldier", 40.474, 0.750, -5.293)
+
+-- [[ Wave 5 ]] --
+PSH:Upgrade(4)
+PSH:Upgrade(4)
+PSH:Upgrade(5)
+PSH:Upgrade(2)
+PSH:Upgrade(1)
+
+-- [[ Wave 6 ]] --
+PSH:Upgrade(3)
+PSH:Place("Scout", 48.735, 0.750, -7.802)
+PSH:Place("Scout", 51.844, 0.750, -7.544)
+PSH:Upgrade(7)
+PSH:Upgrade(7)
+PSH:Upgrade(6)
+PSH:Upgrade(6)
+
+-- [[ Wave 7 ]] --
+PSH:Upgrade(5)
+
+-- [[ Wave 8 ]] --
+PSH:Upgrade(2)
+PSH:Upgrade(3)
+
+-- [[ Wave 9 ]] --
+PSH:Place("Scout", 52.303, 0.750, -1.578)
+PSH:Place("Scout", 49.301, 0.750, -2.097)
+PSH:Place("Scout", 48.767, 0.750, 0.942)
+PSH:Place("Scout", 47.188, 0.750, -10.406)
+PSH:Upgrade(11)
+PSH:Upgrade(11)
+PSH:Upgrade(8)
+PSH:Upgrade(8)
+
+-- [[ Wave 10 ]] --
+PSH:Upgrade(9)
+PSH:Upgrade(9)
+PSH:Upgrade(10)
+PSH:Upgrade(10)
+PSH:Place("Soldier", 43.044, 0.750, -3.650)
+
+-- [[ Wave 11 ]] --
+PSH:Place("Soldier", 40.402, 0.750, -2.080)
+PSH:Place("Soldier", 37.530, 0.750, -0.611)
+PSH:Place("Soldier", 35.673, 0.750, 2.088)
+PSH:Place("Soldier", 38.817, 0.750, 2.157)
+PSH:Place("Soldier", 42.483, 0.750, 0.653)
+PSH:Place("Soldier", 45.666, 0.750, 1.739)
+PSH:Place("Soldier", 41.769, 0.750, 3.719)
+PSH:Place("Soldier", 39.016, 0.750, 5.396)
+PSH:Place("Soldier", 35.935, 0.750, 5.266)
+PSH:Place("Soldier", 37.235, 0.750, 8.155)
+
+-- [[ Wave 12 ]] --
+PSH:Upgrade(18)
+PSH:Upgrade(18)
+PSH:Upgrade(19)
+PSH:Upgrade(19)
+PSH:Upgrade(17)
+PSH:Upgrade(17)
+PSH:Upgrade(12)
+PSH:Upgrade(12)
+PSH:Upgrade(13)
+PSH:Upgrade(13)
+PSH:Upgrade(14)
+
+-- [[ Wave 13 ]] --
+PSH:Upgrade(14)
+PSH:Upgrade(15)
+PSH:Upgrade(15)
+PSH:Upgrade(16)
+PSH:Upgrade(16)
+PSH:Upgrade(20)
+PSH:Upgrade(21)
+PSH:Upgrade(22)
+PSH:Upgrade(22)
+PSH:Upgrade(21)
+PSH:Upgrade(20)
+
+-- [[ Wave 14 ]] --
+PSH:Upgrade(6)
+PSH:Upgrade(9)
+
+-- [[ Wave 15 ]] --
+PSH:Upgrade(8)
+PSH:Upgrade(7)
+PSH:Upgrade(10)
+PSH:Upgrade(11)
+
+-- [[ Wave 16 ]] --
+PSH:Upgrade(2)
+PSH:Upgrade(3)
+PSH:Upgrade(1)
+PSH:Upgrade(5)
+
+-- [[ Wave 17 ]] --
+PSH:Upgrade(4)
+PSH:Upgrade(12)
+
+-- [[ Wave 18 ]] --
+PSH:Upgrade(13)
+PSH:Upgrade(14)
+PSH:Upgrade(17)
+
+-- [[ Wave 19 ]] --
+PSH:Upgrade(16)
+PSH:Upgrade(15)
+PSH:Upgrade(21)
+PSH:Upgrade(22)
+PSH:Upgrade(20)
+PSH:Upgrade(19)
+
+-- [[ Wave 20 ]] --
+PSH:Upgrade(18)
+PSH:Upgrade(10)
+PSH:Upgrade(9)
+PSH:Upgrade(11)
+PSH:Upgrade(6)
+PSH:Upgrade(7)
+PSH:Upgrade(8)
+PSH:Upgrade(2)
+PSH:Upgrade(4)
+PSH:Upgrade(5)
+PSH:Upgrade(1)
