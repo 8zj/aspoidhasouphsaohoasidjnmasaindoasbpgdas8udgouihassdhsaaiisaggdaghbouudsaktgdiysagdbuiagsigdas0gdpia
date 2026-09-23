0109d5a192c44ae2b3d064f49b052c03c8c2ffd9
Pick <90311538+8zj@users.noreply.github.com>
2026-09-23 16:26:42 -0400

    Create Pier Pressure.lua

---
+++ EasyMode/Pier Pressure.lua
@@ -0,0 +1,159 @@
+
+-- [[ Wave 1 ]] --
+PSH:Place("Scout", 26.018, 1.018, -13.574)
+PSH:Place("Scout", 22.598, 1.019, -13.333)
+PSH:Place("Scout", 19.226, 1.019, -13.444)
+PSH:Upgrade(3)
+PSH:Upgrade(2)
+PSH:Upgrade(1)
+
+-- [[ Wave 2 ]] --
+PSH:Place("Scout", 26.293, 1.020, -7.351)
+PSH:Place("Scout", 22.974, 1.020, -7.197)
+
+-- [[ Wave 3 ]] --
+PSH:Place("Scout", 19.914, 1.020, -7.223)
+PSH:Upgrade(6)
+PSH:Upgrade(5)
+PSH:Upgrade(4)
+
+-- [[ Wave 4 ]] --
+PSH:Place("Scout", 16.206, 1.019, -13.410)
+PSH:Upgrade(7)
+PSH:Place("Scout", 16.838, 1.020, -7.655)
+PSH:Upgrade(8)
+PSH:Upgrade(8)
+PSH:Upgrade(7)
+
+-- [[ Wave 5 ]] --
+PSH:Upgrade(3)
+PSH:Upgrade(6)
+PSH:Upgrade(5)
+PSH:Upgrade(2)
+
+-- [[ Wave 6 ]] --
+PSH:Upgrade(1)
+PSH:Upgrade(4)
+
+-- [[ Wave 7 ]] --
+PSH:Place("Scout", 17.604, 1.018, -16.223)
+PSH:Place("Scout", 20.724, 1.018, -16.185)
+PSH:Place("Scout", 23.842, 1.018, -16.078)
+PSH:Upgrade(11)
+PSH:Upgrade(11)
+PSH:Upgrade(10)
+PSH:Upgrade(10)
+PSH:Upgrade(9)
+PSH:Upgrade(9)
+
+-- [[ Wave 8 ]] --
+PSH:Place("Scout", 18.094, 1.020, -4.670)
+PSH:Upgrade(12)
+PSH:Upgrade(12)
+PSH:Place("Scout", 21.267, 1.020, -4.394)
+PSH:Upgrade(13)
+PSH:Upgrade(13)
+PSH:Place("Scout", 24.691, 1.020, -4.621)
+PSH:Upgrade(14)
+PSH:Upgrade(14)
+PSH:Place("Scout", 13.288, 1.019, -14.968)
+
+-- [[ Wave 9 ]] --
+PSH:Upgrade(15)
+PSH:Upgrade(15)
+PSH:Place("Scout", 13.531, 1.020, -7.900)
+PSH:Upgrade(16)
+PSH:Upgrade(16)
+PSH:Place("Scout", 14.926, 1.020, -5.005)
+PSH:Upgrade(17)
+PSH:Upgrade(17)
+
+-- [[ Wave 10 ]] --
+PSH:Place("Scout", 27.014, 1.017, -16.629)
+PSH:Upgrade(18)
+PSH:Upgrade(18)
+PSH:Place("Scout", 24.856, 1.009, -18.990)
+PSH:Upgrade(19)
+PSH:Upgrade(19)
+PSH:Place("Scout", 21.822, 1.017, -19.044)
+PSH:Upgrade(20)
+PSH:Upgrade(20)
+PSH:Place("Scout", 18.621, 1.017, -19.424)
+PSH:Upgrade(21)
+PSH:Upgrade(21)
+
+-- [[ Wave 11 ]] --
+PSH:Upgrade(9)
+PSH:Upgrade(3)
+PSH:Upgrade(10)
+PSH:Upgrade(2)
+
+-- [[ Wave 12 ]] --
+PSH:Upgrade(11)
+PSH:Upgrade(1)
+
+-- [[ Wave 13 ]] --
+PSH:Upgrade(4)
+PSH:Upgrade(5)
+
+-- [[ Wave 14 ]] --
+PSH:Upgrade(6)
+PSH:Upgrade(16)
+PSH:Upgrade(17)
+PSH:Upgrade(12)
+
+-- [[ Wave 15 ]] --
+PSH:Upgrade(13)
+PSH:Upgrade(14)
+PSH:Upgrade(7)
+PSH:Upgrade(9)
+
+-- [[ Wave 16 ]] --
+PSH:Upgrade(3)
+PSH:Upgrade(10)
+
+-- [[ Wave 17 ]] --
+PSH:Upgrade(2)
+PSH:Upgrade(6)
+
+-- [[ Wave 18 ]] --
+PSH:Upgrade(8)
+PSH:Upgrade(16)
+PSH:Upgrade(8)
+PSH:Upgrade(12)
+
+-- [[ Wave 19 ]] --
+PSH:Upgrade(13)
+PSH:Upgrade(5)
+PSH:Upgrade(14)
+PSH:Upgrade(4)
+PSH:Upgrade(21)
+
+-- [[ Wave 20 ]] --
+PSH:Upgrade(20)
+PSH:Upgrade(19)
+PSH:Upgrade(18)
+PSH:Upgrade(11)
+PSH:Upgrade(19)
+PSH:Upgrade(20)
+PSH:Upgrade(21)
+PSH:Place("Scout", 1.730, 1.019, -14.957)
+PSH:Place("Scout", 4.855, 1.019, -14.992)
+PSH:Upgrade(23)
+PSH:Upgrade(23)
+PSH:Upgrade(23)
+PSH:Upgrade(23)
+PSH:Upgrade(22)
+PSH:Upgrade(22)
+PSH:Upgrade(22)
+PSH:Upgrade(22)
+PSH:Place("Scout", 3.610, 1.018, -18.323)
+PSH:Upgrade(24)
+PSH:Upgrade(24)
+PSH:Upgrade(24)
+PSH:Upgrade(24)
+PSH:Place("Scout", 0.208, 1.018, -17.567)
+PSH:Upgrade(25)
+PSH:Upgrade(25)
+PSH:Upgrade(25)
+PSH:Upgrade(25)
